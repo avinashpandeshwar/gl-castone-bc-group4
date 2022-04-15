99d27293d23af4ef604ef3222ffcbe2a1550985f
"avinashpandeshwar" <"avinashpandeshwar@gmail.com">
2022-04-15 17:21:38 +0530

    Implemented book_ticket() and calcel_booking() with more modifiers

--- contracts/Airline.sol
+++ contracts/Airline.sol
@@ -16,12 +16,32 @@ contract Airline {
         FlightStatus status;
     }
 
+    struct TicketData {
+        string flight_name;
+        uint no_of_seats;
+    }
+
+    mapping(address => TicketData) internal _tickets;
+
     address private admin;
     string[] internal _flights;
     mapping(string => FlightData) internal _flight_info;
 
     modifier is_admin() {
         require(msg.sender == admin, "Action not permitted for non-Admin users.");
+        _;
+    }
+
+    modifier valid_booking_inputs(address _addr, string memory _flight_name, uint _no_of_seats) {
+        require(_addr != admin, "Seat booking not permitted for Admin users.");
+        require(_flight_info[_flight_name].estimated_departure != 0, "Invalid flight name.");
+        require(_no_of_seats > 0, "Can not book 0 seats.");
+        require(_flight_info[_flight_name].no_of_seats >= _no_of_seats,"Specified number of seats not available.");
+        _;
+    }
+
+    modifier valid_ticket(address _addr) {
+        require(_tickets[_addr].no_of_seats > 0, "Specified ticket address does not exist.");
         _;
     }
 
@@ -64,7 +84,45 @@ contract Airline {
         return _flight_info[_flight_name];
     }
 
-    // function book_ticket() external {
+    function book_ticket(address _customer, string memory _flight_name, uint _no_of_seats) external valid_booking_inputs(_customer, _flight_name, _no_of_seats) returns(address) {
+        
+        // Calculate total price for ticket
+        uint _amount_payable = _no_of_seats * _flight_info[_flight_name].seat_price;
 
-    // }
+        // Deploy Ticket contract
+        address ticket_address = address(new Ticket(_flight_name, _customer, _amount_payable));
+
+        // Build ticket properties
+        TicketData memory ticket_data = TicketData({
+            flight_name: _flight_name,
+            no_of_seats: _no_of_seats
+        });
+
+        // Add ticket address + properties in local _tickets mapping
+        _tickets[ticket_address] = ticket_data;
+
+        // Reduce number of seats available for flight
+        decrement_flight_seats(_flight_name, _no_of_seats);
+
+        return ticket_address;
+    }
+
+    function cancel_booking(address _ticket) external valid_ticket(_ticket) {
+        // Call cancel method for existing Ticket contract (using address)
+        (bool _res,) = _ticket.call(abi.encodeWithSignature("cancel_ticket()"));
+
+        if (_res) {
+
+            // Getting ticket properties using ticket address
+            string memory flight_name = _tickets[_ticket].flight_name;
+            uint no_of_seats = _tickets[_ticket].no_of_seats;
+
+            // Incrementing cancelled flight seats
+            increment_flight_seats(flight_name, no_of_seats);
+
+            // Remove ticket from local _tickets mapping
+            delete _tickets[_ticket];
+
+        }
+    }
 }
--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -2,4 +2,15 @@
 
 pragma solidity ^0.8;
 
-contract Ticket {}
+contract Ticket {
+
+    string _flight_name;
+    address _customer_address;
+    uint _amount_payable;
+
+    constructor(string memory flight_name, address customer, uint amount_payable) {
+        _flight_name = flight_name;
+        _customer_address = customer;
+        _amount_payable = amount_payable;
+    }
+}
